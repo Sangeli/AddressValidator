c05676a94055e12a7b69f37ad3feb594d69db5db
Stephen Cefali <stephencefali@gmail.com>
2017-01-25 10:18:31 -0800

    removing unecessary stuff

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 // set up ======================================================================
 var express = require('express');
 var app = express(); 						// create our app w/ express
-var mongoose = require('mongoose'); 				// mongoose for mongodb
 var port = process.env.PORT || 8080; 				// set the port
-var database = require('./config/database'); 			// load the database config
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
 // configuration ===============================================================
-mongoose.connect(database.localUrl); 	// Connect to local MongoDB instance. A remoteUrl is also available (modulus.io)
 
 app.use(express.static('./public')); 		// set the static files location /public/img will be /img for users
 app.use(morgan('dev')); // log every request to the console
@@ -19,8 +16,14 @@ app.use(bodyParser.json({type: 'application/vnd.api+json'})); // parse applicati
 app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
 
 
-// routes ======================================================================
-require('./app/routes.js')(app);
+
+
+app.get('*', function (req, res) {
+    res.sendFile(__dirname + '/public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+});
+
+
+
 
 // listen (start app with node server.js) ======================================
 app.listen(port);
